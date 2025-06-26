d2620fbb9eefe0ac3b39ee2731755cc85dec63e5
Dalibor Svonavec <xsvonav@fi.muni.cz>
2025-06-26 17:47:34 +0200

    feat: initialize project

---
+++ src/main/java/cz/svonavec/tennis/ReservationApplication.java
@@ -0,0 +1,16 @@
+package cz.svonavec.tennis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@SpringBootApplication
+@EnableTransactionManagement
+public class ReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationApplication.class, args);
+    }
+
+}
